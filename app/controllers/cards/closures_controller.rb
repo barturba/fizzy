ac656feb4b16ee26ab213128f26dd0d2b0f2ab8a
Stanko K.R. <stanko@stanko.io>
2025-12-05 14:07:36 +0100

    Add API for closing and opening cards

--- app/controllers/cards/closures_controller.rb
+++ app/controllers/cards/closures_controller.rb
@@ -3,11 +3,19 @@ class Cards::ClosuresController < ApplicationController
 
   def create
     @card.close(user: Current.user)
-    render_card_replacement
+
+    respond_to do |format|
+      format.turbo_stream { render_card_replacement }
+      format.json { head :no_content }
+    end
   end
 
   def destroy
     @card.reopen(user: Current.user)
-    render_card_replacement
+
+    respond_to do |format|
+      format.turbo_stream { render_card_replacement }
+      format.json { head :no_content }
+    end
   end
 end
